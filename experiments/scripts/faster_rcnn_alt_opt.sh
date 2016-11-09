e6979d396c68e8774ea5e41c9985f8d47885e383
kilho <kim.kilho@sualab.com>
2016-11-09 11:25:09 +0900

    Add a new datasets option to experiment script

--- experiments/scripts/faster_rcnn_alt_opt.sh
+++ experiments/scripts/faster_rcnn_alt_opt.sh
@@ -29,6 +29,12 @@ case $DATASET in
     PT_DIR="pascal_voc"
     ITERS=40000
     ;;
+  sualab_skive)
+    TRAIN_IMDB="sualab_skive_train"
+    TEST_IMDB="sualab_skive_val"
+    PT_DIR="sualab_skive"
+    ITERS=40000
+    ;;
   coco)
     echo "Not implemented: use experiments/scripts/faster_rcnn_end2end.sh for coco"
     exit
